4dbf13475525bfcba01b892584ff905b47844046
mudhafar <mudhaffar2@gmail.com>
2017-08-09 10:26:50 +0200

    work on parseDateInputValue

--- index.js
+++ index.js
@@ -4,22 +4,26 @@ var output = document.querySelector(".date-output");
 
 
 input.addEventListener('keyup', function() {
-  var parsedDate = parseDate(input.value);
-  output.textContent = 'parsed date ' + parsedDate;
+  var parsedDate = parseDateInputValue(input.value);
+  output.textContent = 'parsed date ' + parsedDate.toString();
   // console.log(typeof(input.value));
 });
 
-function parseDate (input) {
+function parseDateInputValue (input) {
 
-  var dayNum = input.substr(0,2);
-  var monthNum = input.substr(3,2);
-  var year = input.
-
-
-
-
+  var date = input.substr(0,2);
+  var month = input.substr(3,2);
+  var year = input.substr(6,4);
+  var hours = input.substr(11,2);
+  var minutes = input.substr(14,2);
 
 
+
+
+
+  var returnedDate = new Date(year, month, date, hours, minutes, 0, 0);
+
+  return returnedDate;
 
 }
 
