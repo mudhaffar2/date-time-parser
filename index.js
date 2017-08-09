424eae1257f2f5935ca4715cd5c25825ca5630da
mudhafar <mudhaffar2@gmail.com>
2017-08-09 11:33:08 +0200

    add input validation

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ var output = document.querySelector(".date-output");
 
 
 input.addEventListener('keyup', function() {
-  var parsedDate = parseDateInputValue(input.value);
-  output.textContent = 'parsed date ' + parsedDate.toString();
-  // console.log(typeof(input.value));
+  var inputVal = input.value;
+  console.log(input.checkValidity());
+  if (!(input.checkValidity())) {
+    output.textContent = "The Format Should Be 'DD-MM-YYYY HH:mm'";
+  } else {
+    var parsedDate = parseDateInputValue(inputVal);
+    output.textContent = 'parsed date ' + parsedDate.toString();
+    // console.log(typeof(input.value));
+  }
 });
 
 function parseDateInputValue (input) {
@@ -16,10 +22,6 @@ function parseDateInputValue (input) {
   var year = input.substr(6,4);
   var hours = input.substr(11,2);
   var minutes = input.substr(14,2);
-
-
-
-
 
   var returnedDate = new Date(year, month, date, hours, minutes, 0, 0);
 
