655c55629fe60dbfd482b138d5f1e423ca779909
mudhafar <mudhaffar2@gmail.com>
2017-08-09 10:08:20 +0200

    add event listener on input

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 
+var input = document.querySelector("input");
+var output = document.querySelector(".date-output");
+
+
+input.addEventListener('keyup', function() {
+  var parsedDate = parseDate(input.value);
+  output.textContent = 'parsed date ' + parsedDate;
+  // console.log(typeof(input.value));
+});
+
+function parseDate (input) {
+
+  var dayNum = input.substr(0,2);
+  var monthNum = input.substr(3,2);
+  var year = input.
+
+
+
+
+
+
+
+}
+
